54bb9ef4214ff6dc1eaabcf93c088e0c690e2d8d
rong5026 <rong5026@naver.com>
2024-04-20 17:49:46 +0900

    csrf disable, register post요청

--- TestSecurity/src/main/java/com/example/testsecurity/config/SecurityConfig.java
+++ TestSecurity/src/main/java/com/example/testsecurity/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.OAuth2ClientDsl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -22,6 +23,7 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
 
 		http
 			.authorizeHttpRequests((auth) -> auth
