fe8a28cc4c5966c9baa343e1a9eb259043d19cf7
rong5026 <rong5026@naver.com>
2024-04-21 20:42:07 +0900

    BctypPasswordEncoder() 사용

--- udemysecurity/src/main/java/com/example/udemysecurity/config/ProjectSecurityConfig.java
+++ udemysecurity/src/main/java/com/example/udemysecurity/config/ProjectSecurityConfig.java
@@ -5,9 +5,11 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -19,62 +21,31 @@ public class ProjectSecurityConfig {
 
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(AbstractHttpConfigurer::disable);  // 이거 안해주면 post요청이 안됨
+
 		http.authorizeHttpRequests(
 			authorize ->
 				authorize
 				.requestMatchers("/myAccount/**", "/myBalance", "/myLoans", "/myCards").authenticated()
-				.requestMatchers("/notices", "/contact", "/login").permitAll()
+				.requestMatchers("/notices", "/contact", "/login", "/register").permitAll()
 			)
 			.formLogin(formLogin -> formLogin.permitAll()
 				// formLogin -> formLogin
 				// .loginPage("/login")
 				// .permitAll()
-			);
+			)
+			;
 		return http.build();
 
 	}
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+		return new BCryptPasswordEncoder();
 	}
 
-	// EazyBackUserDetails에 이미 UserDetailsService를 사용하므로 주석처리 안하면 충돌 발생
-	// @Bean
-	// public UserDetailsService userDetailsService(DataSource dataSource) {
-	// 	return new JdbcUserDetailsManager(dataSource);
-	// }
 
-	// @Bean
-	// public InMemoryUserDetailsManager userDetailsManager() {
-	//
-	// 	// // 첫번째 방법. 사용 권장하지 않음
-	// 	// UserDetails admin = User.withDefaultPasswordEncoder()
-	// 	// 	.username("admin")
-	// 	// 	.password("1234")
-	// 	// 	.authorities("admin")
-	// 	// 	.build();
-	// 	//
-	// 	// UserDetails user = User.withDefaultPasswordEncoder()
-	// 	// 	.username("user")
-	// 	// 	.password("1234")
-	// 	// 	.authorities("read")
-	// 	// 	.build();
-	//
-	// 	// 두번째 방법.
-	// 	UserDetails admin = User.withUsername("admin")
-	// 		.password("1234")
-	// 		.authorities("admin")
-	// 		.build();
-	//
-	// 	UserDetails user = User.withUsername("user")
-	// 		.password("1234")
-	// 		.authorities("read")
-	// 		.build();
-	//
-	//
-	// 	return new InMemoryUserDetailsManager(admin, user);
-	// }
 
 
 }
--- udemysecurity/src/main/java/com/example/udemysecurity/entity/Customer.java
+++ udemysecurity/src/main/java/com/example/udemysecurity/entity/Customer.java
@@ -10,8 +10,8 @@ import jakarta.persistence.Id;
 @Entity
 public class Customer {
 	@Id
-	@GeneratedValue(strategy= GenerationType.AUTO,generator="native")
-	@GenericGenerator(name = "native",strategy = "native")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	// @GenericGenerator(name = "native",strategy = "native")
 	private int id;
 	private String email;
 	private String pwd;
